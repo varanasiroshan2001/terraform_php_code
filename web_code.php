f222a748dc602a1327dbd58e18f3327a71da7eef
varanasiroshan2001 <64471180+varanasiroshan2001@users.noreply.github.com>
2020-06-16 18:13:17 +0530

    main code for website

---
+++ web_code.php
@@ -0,0 +1,20 @@
+<body bgcolor='#elelel'>
+<h1>THIS IS MY AMAZON WEBSITE WORKING ON CLOUD</h1>
+
+<video width="750" height="590" controls>
+  <source src="https://mys31111.s3.ap-south-1.amazonaws.com/videoplayback.mp4" type="video/mp4">
+</video>
+<img src='https://mys31111.s3.ap-south-1.amazonaws.com/WhatsApp+Image+2020-06-14+at+1.52.33+AM.jpeg' height=560 width=550 />
+
+<pre>
+
+<?php
+
+print `ifconfig\n`;
+
+print `date`;
+
+print `cal`;
+
+
+?>
